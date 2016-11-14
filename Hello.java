883e0b9874bf6f14979e1ef19928d7db80d714c5
natthawut thammajark <tiwnatthawut1993>
2016-11-14 15:10:44 +0700

    5

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
-	public static void main(String args[])
+	public static void main(String args[]){
 		System.out.println(" Hello word");
+		System.out.println(" Hello travis");
 		
 	}
 }
